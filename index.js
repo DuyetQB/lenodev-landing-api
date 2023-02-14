60cf00a490f941311d91d63d96b1d5f19f8a158b
DESKTOP-RONIHRQ\Legion 5 <dinhsyduyet@gmail.com>
2023-02-14 19:16:01 +0700

    rm cor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const router = require("./router/index.js");
 require('dotenv').config()
-const PORT = 3003
+const PORT = 3004
 const morgan = require('morgan');
 
 
@@ -13,19 +13,17 @@ const morgan = require('morgan');
 //         useNewUrlParser: true,
 //         useUnifiedTopology: true
 // });
-// let corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
 let corsOptions = {
-    origin: 'https://lenodev.com',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
-  
-// app.use(express.json({limit: '50mb'}));
-// app.use(express.urlencoded({limit: '50mb'}));
+// let corsOptions = {
+//     origin: 'https://lenodev.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+
 app.use(express.json({extended:false}));
-app.use(cors());
+ app.use(cors());
 app.use(morgan("dev"));
 
 // mongodb+srv://dinhsyduyet:<password>@cluster0.hy4q0.mongodb.net/?retryWrites=true&w=majority
@@ -41,7 +39,7 @@ mongoose.connect(Url, { useNewUrlParser: true,useUnifiedTopology: true })
 
 
 
-app.use("/",cors(corsOptions),router);
+app.use("/",router);
 
 app.listen(PORT,function(){
     console.log(`app is runing on port-${PORT} `);
